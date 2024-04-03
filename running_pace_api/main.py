e190271842526bea0d9cef163293ccd7eb1e35a5
Cyril Moron <cyril.moron@gmail.com>
2024-04-03 20:05:42 +0200

    Update requirement.txt. Update allowed origins for CORS

--- running_pace_api/main.py
+++ running_pace_api/main.py
@@ -12,7 +12,9 @@ app = FastAPI()
 
 allowed_origins = [
     "http://localhost:5173",
-    "http://127.0.0.1:5173"
+    "http://127.0.0.1:5173",
+    "http://allures.moron.at",
+    "https://allures.moron.at",
 ]
 
 app.add_middleware(
