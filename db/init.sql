dad030093e1167601104dfb70bb8928b8abcaeee
Cyril Moron <cyril.moron@gmail.com>
2024-05-14 23:29:29 +0200

    Add Trigram Indexing and Extension to PostgreSQL Initialization
    - Updated .gitignore to ignore generated CSV files in the db directory.
    - Modified init.sql to include the creation of the pg_trgm extension.
    - Added commands to create a trigram index on the 'name' field in the 'athletes' table using the pg_trgm extension.
    - These changes improve the performance of text search queries on the 'name' field in the 'athletes' table.

--- db/init.sql
+++ db/init.sql
@@ -19,3 +19,10 @@ CREATE TABLE IF NOT EXISTS athletes (
 -- Importation des données
 COPY clubs FROM '/var/lib/postgresql/db/clubs.csv' DELIMITER ',' CSV HEADER;
 COPY athletes FROM '/var/lib/postgresql/db/athletes.csv' DELIMITER ',' CSV HEADER;
+
+-- Création de l'extension pg_trgm pour l'indexation trigram
+CREATE EXTENSION IF NOT EXISTS pg_trgm;
+
+-- Création de l'index trigram sur le champ name
+DROP INDEX IF EXISTS idx_athletes_name_trgm;
+CREATE INDEX idx_athletes_name_trgm ON athletes USING gin (lower(name) gin_trgm_ops)
